5c82912608fbe267707256044b34a585231ec06c
Nastia Chan <nc624@ic.ac.uk>
2024-11-04 14:35:31 +0000

    add username

--- app_test.py
+++ app_test.py
@@ -10,3 +10,7 @@ def test_knows_about_dinosaurs():
 
 def test_does_not_know_about_asteroids():
     assert process_query("asteroids") == "Unknown"
+
+
+def test_query_about_name_returns_player_name():
+    assert process_query("What is your name?") == "nc624"
